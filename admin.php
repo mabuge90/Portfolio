cdae40ecbbbde9fa11ee6951c288eafe736d0cce
Weylon Neil Bulloch <panatha_4ever@hotmail.com>
2019-03-18 16:53:03 +0000

    created css file for admin panel page and populated table with data from db

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 $db = new PDO("mysql:host=192.168.20.20;dbname=Portfolio", 'root', '');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$sql = 'SELECT `title`, `img_url` FROM `projects`;';
+$sql = 'SELECT `title`, `img_url`, `date_added` FROM `projects`;';
 
 $query = $db->prepare($sql);;
 $query->execute();
@@ -12,13 +12,50 @@ $result = $query->fetchAll();
 
 <!DOCTYPE html>
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/adminStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+    <title></title>
+</head>
 <body>
+<h1>Admin Panel</h1>
+<div class="section_break"></div>
 <table>
+<thead>
     <tr>
-        <th>Title</th>
+        <th>Project Title</th>
+
         <th>Image URL</th>
+
         <th>Date Added</th>
+
+        <th></th>
+
+        <th></th>
     </tr>
+</thead>
+<tbody>
+
+<?php
+    foreach ($result as $project) {
+
+        echo '<tr><td>' . $project['title'] . '</td>';
+
+        echo '<td>' . $project['img_url'] . '</td>';
+
+        echo '<td>' . $project['date_added'] . '</td>';
+
+        echo '<td><input class = "edit" type="submit" value = "Edit"></td>';
+
+        echo '<td><input class = "delete" type="submit" value = "Delete"</td></tr>';
+         
+    }
+
+?>
+
+</tbody>
+
+
 </table>
 
 </body>
