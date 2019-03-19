dd0789dfbeea0b0ed54b36903d2ba05af6c3e534
Weylon Neil Bulloch <panatha_4ever@hotmail.com>
2019-03-19 09:51:55 +0000

    created form page to add new project, created css for form page, added upload file functionality and upload folder

---
+++ newProject.php
@@ -0,0 +1,40 @@
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="CSS/newProject.css">
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+    <title>Add new entry</title>
+</head>
+<body>
+<div class="logo">
+    <img src="Images/logo_transparent.png" alt="logo">
+</div>
+<h1>Add new entry</h1>
+
+<div class="addNewForm">
+
+    <form method = "POST" action="upload.php" enctype="multipart/form-data">
+        <h3>Project Title: </h3>
+        <input type="text" name = "title" placeholder="Enter title">
+        <h3>Select image file to upload: </h3>
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+
+    <?php
+    echo '<br>';
+    require_once ('upload.php');
+    ?>
+
+</div>
+
+</body>
+</html>
+
