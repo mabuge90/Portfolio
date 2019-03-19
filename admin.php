6c80e28e63b7af087ccd3a7b46438eec8157f6c9
Weylon Neil Bulloch <panatha_4ever@hotmail.com>
2019-03-19 11:09:58 +0000

    fixed spaces and css classes

--- admin.php
+++ admin.php
@@ -7,7 +7,6 @@ $sql = 'SELECT `title`, `img_url`, `date_added` FROM `projects`;';
 $query = $db->prepare($sql);;
 $query->execute();
 $result = $query->fetchAll();
-
 ?>
 
 <!DOCTYPE html>
@@ -37,15 +36,15 @@ $result = $query->fetchAll();
         echo '<tr><td>' . $project['title'] . '</td>';
         echo '<td>' . $project['img_url'] . '</td>';
         echo '<td>' . $project['date_added'] . '</td>';
-        echo '<td><input class = "edit" type="submit" value = "Edit"></td>';
-        echo '<td><input class = "delete" type="submit" value = "Delete"</td></tr>';
+        echo '<td><input class="adminButton edit" type="submit" value="Edit"></td>';
+        echo '<td><input class="adminButton delete" type="submit" value= "Delete"</td></tr>';
     }
 ?>
 </tbody>
 </table>
 <table class="newProject">
     <tr>
-        <th><input class="createNew" type = "submit" value="Add new entry   +"</th>
+        <th><input class="createNew" type="submit" value="Add new entry"</th>
     </tr>
 </table>
 
